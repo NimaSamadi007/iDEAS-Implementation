Fix overflow bug - Save DVFS model - Achieve acceptable results

dvfs/dvfs.py
@@ -81,6 +81,7 @@ class DVFS:
                  eps_decay: float = 1.0/2000,
                  max_eps: float = 1.0,
                  min_eps: float = 0.1,
+                 eps_update_step: int = 1e3,
                  gamma: float = 0.9,
                  weight_decay: float = 1e-5):
 
@@ -99,12 +100,14 @@ class DVFS:
         self.repl_buf = ReplayBuffer(state_dim, mem_size, batch_size)
         self.batch_size = batch_size
         self.eps = max_eps
+        self.eps_update_step = eps_update_step
         self.eps_decay = eps_decay
         self.max_eps = max_eps
         self.min_eps = min_eps
+        self.eps_update_cnt = 0
         self.update_target_net = update_target_net
         self.gamma = gamma
-        self.update_cnt = 1
+        self.update_cnt = 0
         self.losses = []
 
         # Training device
@@ -138,18 +141,21 @@ class DVFS:
         if len(self.repl_buf) >= self.batch_size:
             loss = self.update_model()
             self.losses.append(loss)
-            self.update_cnt += 1
 
             # decrease epsilon
-            self.eps = max(
-                self.min_eps, self.eps-self.eps_decay*(self.max_eps-self.min_eps)
-            )
+            self.eps_update_cnt += 1
+            if self.eps_update_cnt % self.eps_update_step == 0:
+                self.eps_update_cnt = 0
+                self.eps = max(
+                    self.min_eps, self.eps-self.eps_decay*(self.max_eps-self.min_eps)
+                )
+
+            self.update_cnt += 1
             if self.update_cnt % self.update_target_net == 0:
-                self.update_cnt = 1
+                self.update_cnt = 0
                 self._update_target_net()
             return loss
         return None
-
 
     def execute(self, states: np.ndarray) -> np.ndarray:
         actions = []
@@ -174,6 +180,17 @@ class DVFS:
             act = self._conv_act_id_to_type(act)
             conv_actions[act[0]].append([i, act[1]])
         return conv_actions
+
+    # Evaluate the DVFS model on the given tasks
+    def evaluate(self, states: np.ndarray):
+        pass
+
+    def save_model(self, path: str):
+        torch.save(self.net.state_dict(), path)
+
+    def load_pretrained_model(self, path_to_model: str):
+        self.net.load_state_dict(torch.load(path_to_model))
+        self.target_net.load_state_dict(self.net.state_dict())
 
     def _compute_net_loss(self, samples: Dict[str, np.ndarray]) -> torch.Tensor:
         state = torch.FloatTensor(samples["state"]).to(self.device)

models/env.py
@@ -106,7 +106,7 @@ class TOMSEnv:
     def observe(self):
         self.curr_tasks = self.task_gen.step()
         self.curr_state = self._get_system_state()
-        is_final = len(self.curr_tasks)*[False]
+        is_final = len(self.curr_tasks)*[True]
 
         return self.curr_state, is_final
 
@@ -126,19 +126,26 @@ class TOMSEnv:
         min_penalties = []
         for task in self.curr_tasks:
             if task.deadline_missed:
-                penalty = DEADLINE_MISSED_PENALTY
+                penalty = -1
+                min_penalty = -1
             else:
                 penalty = (task.cons_energy+LATENCY_ENERGY_COEFF*task.aet)
+                min_penalty = np.min([self.cpu.get_min_energy(task),
+                                      self.w_inter.get_cons_energy(task)])
             penalties.append(penalty)
-
-            min_penalty = np.min([self.cpu.get_min_energy(task),
-                                  self.w_inter.get_cons_energy(task)])
             min_penalties.append(min_penalty)
 
         # Calculate reward
         min_penalties = np.asarray(min_penalties, dtype=float)
         penalties = np.asarray(penalties, dtype=float)
+        # print(penalties)
+        # print(min_penalties)
+        if len(np.where(penalties < min_penalties)[0]):
+            raise ValueError("Impossible to reach here!")
+
         rewards = np.exp(-REWARD_COEFF*(penalties-min_penalties))
+        rewards[min_penalties < 0] = -1
+
         return rewards, penalties, min_penalties
 
     def _get_system_state(self):

toms_main.py
@@ -21,8 +21,9 @@ if __name__ == '__main__':
                     act_space=env.get_action_space(),
                     batch_size=32,
                     gamma=0.90,
-                    update_target_net= 1000,
-                    eps_decay = 1/2000,
+                    update_target_net=1000,
+                    eps_update_step=2000,
+                    eps_decay = 1e-2,
                     min_eps=0)
 
     all_rewards = []
@@ -31,7 +32,7 @@ if __name__ == '__main__':
 
     # Initial state observation
     state, _ = env.observe()
-    for itr in range(int(1e4)):
+    for itr in range(int(2.5e5)):
         # Run DVFS to assign tasks
         actions = dvfs_alg.execute(state)
         actions_str = dvfs_alg.conv_acts(actions)
@@ -41,10 +42,10 @@ if __name__ == '__main__':
         next_state, is_final = env.observe()
         # Update RL network
         loss = dvfs_alg.train(state,
-                              actions,
-                              rewards,
-                              next_state,
-                              is_final)
+                            actions,
+                            rewards,
+                            next_state,
+                            is_final)
         # Update current state
         state = next_state
 
@@ -54,19 +55,21 @@ if __name__ == '__main__':
         all_min_penalties.append(min_penalties.tolist())
         if (itr+1) % 500 == 0:
             print(f"At {itr+1}, loss={loss:.3f}")
-            print(f"Actions: {actions_str}")
+            print(f"eps: {dvfs_alg.eps:.2f}")
             print(f"Rewards: {rewards}")
             print(f"Penalties: {penalties}")
             print(f"Min penalties: {min_penalties}")
             print(10*"-")
 
+    # Save model:
+    dvfs_alg.save_model("models/TOMS/dvfs.pt")
+
+    # Plot results
     all_rewards = np.array(all_rewards)
     all_penalties = np.array(all_penalties)
     all_min_penalties = np.array(all_min_penalties)
 
-    # Plot results
-    print(f"Current eps val: {dvfs_alg.eps}")
     plot_all_rewards(all_rewards)
     plot_loss_function(dvfs_alg.losses)
-    for i in range(4):
+    for i in range(all_penalties.shape[1]):
         plot_penalty(all_penalties[:, i], all_min_penalties[:, i], i)