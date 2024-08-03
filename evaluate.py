Add more general training task model - Fix energy calculation bug of DVFS

evaluate.py
@@ -1,21 +1,24 @@
 import numpy as np
 from tqdm import tqdm
+import copy
 
 from env_models.env import Env, RRLOEnv
 from dvfs.dqn_dvfs import DQN_DVFS
+from env_models.task import TaskGen
 from dvfs.rrlo_dvfs import RRLO_DVFS
 from configs import DQN_STATE_DIM
 
 
 def evaluate():
     # Load pre-trained models
-    env_configs = {
+    configs = {
         "task_set": "configs/task_set_eval.json",
         "cpu_local": "configs/cpu_local.json",
         "w_inter": "configs/wireless_interface.json",
     }
-    dqn_env = Env(env_configs)
-    rrlo_env = RRLOEnv(env_configs)
+    task_gen = TaskGen(configs["task_set"])
+    dqn_env = Env(configs, task_gen.get_wcet_bound(), task_gen.get_task_size_bound())
+    rrlo_env = RRLOEnv(configs)
 
     dqn_dvfs = DQN_DVFS(state_dim=DQN_STATE_DIM, act_space=dqn_env.get_action_space())
     rrlo_dvfs = RRLO_DVFS(
@@ -36,8 +39,9 @@ def evaluate():
     rrlo_task_energy_cons = np.zeros(4)
     rrlo_num_tasks = np.zeros(4)
 
-    dqn_state, _ = dqn_env.observe()
-    rrlo_state, _ = rrlo_env.observe()
+    tasks = task_gen.step()
+    dqn_state, _ = dqn_env.observe(copy.deepcopy(tasks))
+    rrlo_state, _ = rrlo_env.observe(copy.deepcopy(tasks))
     for _ in tqdm(range(5000)):
         actions_dqn = dqn_dvfs.execute(dqn_state, eval_mode=True)
         actions_dqn_str = dqn_dvfs.conv_acts(actions_dqn)
@@ -57,8 +61,9 @@ def evaluate():
                 rrlo_task_energy_cons[j.t_id] += j.cons_energy
             rrlo_num_tasks[j.t_id] += len(jobs)
 
-        next_state_dqn, _ = dqn_env.observe()
-        next_state_rrlo, _ = rrlo_env.observe()
+        tasks = task_gen.step()
+        next_state_dqn,_ = dqn_env.observe(copy.deepcopy(tasks))
+        next_state_rrlo,_ = rrlo_env.observe(copy.deepcopy(tasks))
 
         # Update current state
         dqn_state = next_state_dqn